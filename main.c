f14382b22c59d83c17f5508b763af58916a6972b
AVIMIR <rgr.rg.14@mail.ru>
2024-11-14 15:48:08 +0100

    Wrote a bunch of funtions for playGame option (probably should add "PG" prefix to them not to confuse them later on)

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ const int BOARD_WIDTH = 7;
 const int BOARD_HEIGHT = 6;
 
 void displayMainMenu() {
+	// Self-explanatory
 	printf("Welcome to the Connect four!\n\n");
 	printf("What would you like to do:\n");
 	printf("1) Play a new game\n");
@@ -29,8 +30,63 @@ int getProperInput() {
 	return input;
 }
 
+int putDotOnField(int column, int turn) {}
+int saveGame() {}
+int getPlayersName() {}
+int getPlayersNames() {}
+int initializeBoard() {}
+int saveResult() {}
+int getPlaysersInput() {}
+int initializeGame(char *playersName1, char *playersName2) {
+	system("clear");
+	getPlayersNames();
+	initializeBoard();
+
+	return SUCCESSFUL_EXECUTION;
+}
+int handlePlayersInput(int playersInput, turn) {
+	switch (playersInput) {
+		case 0:
+			saveGame(turn);
+		break;
+		default:
+			putDotOnField(playersInput, turn);
+	}
+	return SUCCESSFUL_EXECUTION;
+}
+
+int displayBoard() {
+
+	return SUCCESSFUL_EXECUTION;
+}
+
+int checkWinCondition() {}
+int checkDrawCondition() {}
+int checkGameEnd() {
+	checkWinCondition();
+	checkDrawCondition();
+
+	return SUCCESSFUL_EXECUTION;
+}
+
+int simulateGame() {
+	int turn = 1, playersInput;
+	while (1) {
+		playersInput = getPlaysersInput();
+		handlePlayersInput(playersInput, turn);
+
+		turn *= (-1);
+	}
+	return SUCCESSFUL_EXECUTION;
+}
+
 void playGame() {
-	printf("We played a game. You won!\n");
+	// printf("We played a game. You won!\n");
+	char *playerName1;
+	char *playerName2;
+	initializeGame();
+
+
 }
 
 void loadGame() {
@@ -41,8 +97,11 @@ void exitGame() {
 	printf("Thank you for playing! See you next time.\n");
 }
 
-void handleChoice(int input) {
-	switch (input) {
+void handleChoice(int option) {
+	//
+	// Encapsulating switch block to make main function less crowded.
+
+	switch (option) {
 		case 1:
 			playGame();
 			break;
@@ -57,9 +116,9 @@ void handleChoice(int input) {
 
 int main(void) {
 	displayMainMenu();
-	int input = getProperInput();
-	printf("You entered: %d\n", input);
-	handleChoice(input);
+	int inputOption = getProperInput();
+	// printf("You entered: %d\n", inputOption);
+	handleChoice(inputOption);
 	for (;;) {
 		printf("Would you like to play again? (y/n) : ");
 	}
