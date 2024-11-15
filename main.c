e6645e24289c44d008e864e0ef05f2206181bfd9
AVIMIR <rgr.rg.14@mail.ru>
2024-11-15 15:25:24 +0100

    The forth commit: one can enter their name and exit the game. Does anybody need more? ITS PERFECT!! (next time >> simulating game: putDotOnField)

--- main.c
+++ main.c
@@ -7,7 +7,135 @@ const int SUCCESSFUL_EXECUTION = 0;
 const int BOARD_WIDTH = 7;
 const int BOARD_HEIGHT = 6;
 
-void displayMainMenu() {
+const int MAX_PLAYER_NAME_LENGTH = 31;
+const int PLAYER_COUNT = 2;
+
+
+int SimGdisplayBoard(char board[BOARD_HEIGHT][BOARD_WIDTH]) {
+	for (int i = 0; i < BOARD_HEIGHT; i++) {
+		for (int j = 0; j < BOARD_WIDTH; j++) {
+			printf("%c", board[i][j]);
+		}
+		printf("\n");
+	}
+	printf("-------\n");
+	return SUCCESSFUL_EXECUTION;
+}
+
+char PDOFcalculateSign(int turn) {
+	switch (turn) {
+		case 1:
+			return 'X';
+		case -1:
+			return 'O';
+	}
+}
+
+int CICHECcheckIfCellIsEmpty(char cell) {
+	return !(cell == 'X' || cell == 'O');
+}
+
+int PDOFcheckIfColumnHasEmptyCell(char board[BOARD_HEIGHT][BOARD_WIDTH], int column) {
+	int cellIsEmpty = CICHECcheckIfCellIsEmpty(board[column]);
+	for (int i = BOARD_HEIGHT-1; i >= 0 ; i--) {
+		if (cellIsEmpty)
+			return i;
+	}
+	return -1;
+}
+
+int SimGputDotOnField(char board[BOARD_HEIGHT][BOARD_WIDTH], int column, int turn) {
+	char sign = PDOFcalculateSign(turn);
+	int columnHasEmptyCell;
+	while (1) {
+		columnHasEmptyCell = PDOFcheckIfColumnHasEmptyCell(board, column);
+		if (1)
+			break;
+	}
+
+
+
+	return SUCCESSFUL_EXECUTION;
+}
+int SimGsaveGame() {}
+int PGgetPlayerName(char playersNames[PLAYER_COUNT][MAX_PLAYER_NAME_LENGTH], int playerNumeral) {
+	printf("Please enter player#%d name (max 30 symbols): \n", playerNumeral);
+	scanf("%s", playersNames[playerNumeral]);
+
+	return SUCCESSFUL_EXECUTION;
+}
+int IGgetPlayersNames(char playersNames[PLAYER_COUNT][MAX_PLAYER_NAME_LENGTH]) {
+	for (int i = 0; i < PLAYER_COUNT; i++) {
+		PGgetPlayerName(playersNames, i);
+	}
+
+	return SUCCESSFUL_EXECUTION;
+}
+
+int IGinitializeBoard(char board[BOARD_HEIGHT][BOARD_WIDTH]) {
+	for (int i = 0; i < BOARD_HEIGHT; i++) {
+		for (int j = 0; j < BOARD_WIDTH; j++) {
+			board[i][j] = '.';
+		}
+	}
+}
+
+int IGdisplayPlayersNames(char playersNames[PLAYER_COUNT][MAX_PLAYER_NAME_LENGTH]) {
+	for (int i = 0; i < PLAYER_COUNT; i++) {
+		printf("Player#%d: %s\n", i, playersNames[i]);
+	}
+
+	return SUCCESSFUL_EXECUTION;
+}
+
+int PGsaveResult() {}
+int SimGgetPlaysersInput() {}
+int PGinitializeGame(char playersNames[PLAYER_COUNT][MAX_PLAYER_NAME_LENGTH], char board[BOARD_HEIGHT][BOARD_WIDTH]) {
+	IGgetPlayersNames(playersNames);
+	IGinitializeBoard(board);
+	IGdisplayPlayersNames(playersNames);
+	// PGdisplayBoard(board);
+
+
+
+
+	return SUCCESSFUL_EXECUTION;
+}
+
+int PGcheckWinCondition() {}
+int PGcheckDrawCondition() {}
+int PGcheckGameEndConditions() {
+	PGcheckWinCondition();
+	PGcheckDrawCondition();
+
+	return SUCCESSFUL_EXECUTION;
+}
+
+int PGsimulateGame(char playersNames[PLAYER_COUNT][MAX_PLAYER_NAME_LENGTH], char board[BOARD_HEIGHT][BOARD_WIDTH]) {
+	int turn = 1, playersInput, gameEnd;
+	while (1) {
+		SimGdisplayBoard(board);
+		playersInput = SimGgetPlaysersInput();
+
+		switch (playersInput) {
+			case 0:
+				SimGsaveGame(turn);
+			break;
+			default:
+				SimGputDotOnField(board, playersInput, turn);
+		}
+
+		gameEnd = PGcheckGameEndConditions();
+		if (!gameEnd) {
+			break;
+		}
+		turn *= (-1);
+	}
+	return SUCCESSFUL_EXECUTION;
+}
+
+
+void MMdisplayMainMenu() {
 	// Self-explanatory
 	printf("Welcome to the Connect four!\n\n");
 	printf("What would you like to do:\n");
@@ -16,7 +144,7 @@ void displayMainMenu() {
 	printf("3) Exit the game\n");
 }
 
-int getProperInput() {
+int MMgetProperInput() {
 	// This function asks a user to enter a number from 1 to 3 and if the input is not valid, repeats the request.
 
 	int input = -1;
@@ -30,97 +158,38 @@ int getProperInput() {
 	return input;
 }
 
-int putDotOnField(int column, int turn) {}
-int saveGame() {}
-int getPlayersName() {}
-int getPlayersNames() {}
-int initializeBoard() {}
-int saveResult() {}
-int getPlaysersInput() {}
-int initializeGame(char *playersName1, char *playersName2) {
-	system("clear");
-	getPlayersNames();
-	initializeBoard();
-
-	return SUCCESSFUL_EXECUTION;
-}
-int handlePlayersInput(int playersInput, turn) {
-	switch (playersInput) {
-		case 0:
-			saveGame(turn);
-		break;
-		default:
-			putDotOnField(playersInput, turn);
-	}
-	return SUCCESSFUL_EXECUTION;
-}
-
-int displayBoard() {
-
-	return SUCCESSFUL_EXECUTION;
-}
-
-int checkWinCondition() {}
-int checkDrawCondition() {}
-int checkGameEnd() {
-	checkWinCondition();
-	checkDrawCondition();
-
-	return SUCCESSFUL_EXECUTION;
-}
-
-int simulateGame() {
-	int turn = 1, playersInput;
-	while (1) {
-		playersInput = getPlaysersInput();
-		handlePlayersInput(playersInput, turn);
-
-		turn *= (-1);
-	}
-	return SUCCESSFUL_EXECUTION;
-}
-
-void playGame() {
-	// printf("We played a game. You won!\n");
-	char *playerName1;
-	char *playerName2;
-	initializeGame();
+void MMplayGame(char playersNames[PLAYER_COUNT][MAX_PLAYER_NAME_LENGTH], char board[BOARD_HEIGHT][BOARD_WIDTH]) {
+	PGinitializeGame(playersNames, board);
+	PGsimulateGame(playersNames, board);
 
 
 }
 
-void loadGame() {
+void MMloadGame() {
 	printf("We loaded a game. Now what?\n");
 }
 
-void exitGame() {
+void MMexitGame() {
 	printf("Thank you for playing! See you next time.\n");
 }
 
-void handleChoice(int option) {
-	//
-	// Encapsulating switch block to make main function less crowded.
-
-	switch (option) {
-		case 1:
-			playGame();
-			break;
-		case 2:
-			loadGame();
-			break;
-		case 3:
-			exitGame();
-			exit(SUCCESSFUL_EXECUTION);
-	}
-}
-
 int main(void) {
-	displayMainMenu();
-	int inputOption = getProperInput();
-	// printf("You entered: %d\n", inputOption);
-	handleChoice(inputOption);
-	for (;;) {
-		printf("Would you like to play again? (y/n) : ");
+	char board[BOARD_HEIGHT][BOARD_WIDTH];
+	char playersNames[PLAYER_COUNT][MAX_PLAYER_NAME_LENGTH];
+
+	MMdisplayMainMenu();
+	int inputOption = MMgetProperInput();
+
+	switch (inputOption) {
+		case 1:
+			MMplayGame(playersNames, board);
+		break;
+		case 2:
+			MMloadGame();
+		break;
+		case 3:
+			MMexitGame();
+		exit(SUCCESSFUL_EXECUTION);
 	}
 	return SUCCESSFUL_EXECUTION;
 }
