2f0b3152701b0ae629d766e0b840e5b63798d655
AVIMIR <rgr.rg.14@mail.ru>
2024-11-20 17:21:48 +0100

    The seventh commit: one can save games now and see results of the previous games

--- main.c
+++ main.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+
+#define SUCCESSFUL_EXECUTION 0
+
+#define BOARD_WIDTH 7
+#define BOARD_HEIGHT 6
+#define LENGTH_OF_WINNING_SEQUENCE 4
+
+#define MAX_PLAYER_NAME_LENGTH (30 + 1) // "+1" for null terminator
+#define PLAYER_COUNT 2
+
+#define WIN 2
+#define NO_WIN 0
+
+#define DRAW (-1)
+#define NO_DRAW 0
+#define CONTINUE 0
+
+#define CONNECTED 1
+#define NOT_CONNECTED 0
+
+#define IN_WINNING_SEQUENCE 1
+#define NOT_IN_WINNING_SEQUENCE 0
+
+#define ILLEGAL_MOVE_FULL_COLUMN (-1)
+
+#define SIGN_IN_WINNING_SEQUENCE 'Y'
 
 
 typedef struct{
@@ -8,52 +36,84 @@ typedef struct{
 	int y;
 } Cell;
 
-const int SUCCESSFUL_EXECUTION = 0;
 
-const int BOARD_WIDTH = 7, BOARD_HEIGHT = 6;
+Cell makeCell(char sign, int x, int y) {
+	Cell cell;
+	cell.sign = sign;
+	cell.x = x;
+	cell.y = y;
 
-const int MAX_PLAYER_NAME_LENGTH = 30 + 1; // "+1" for null terminator
-const int PLAYER_COUNT = 2;
+	return cell;
+}
 
-const int WIN = 2, NO_WIN = 0;
-const int DRAW = -1, NO_DRAW = 0;
-const int CONTINUE = 0;
-const int CONNECTED = 1, NOT_CONNECTED = 0;
-const int IN_WINNING_SEQUENCE = 1, NOT_IN_WINNING_SEQUENCE = 0;
 
-const int ILLEGAL_MOVE_FULL_COLUMN = -1;
-char sequenceOnCheck[4];
-Cell winningSequence[4];
+Cell winningSequence[LENGTH_OF_WINNING_SEQUENCE];
+short gameIdIsAcquired = 0;
+unsigned long long gameID;
+char gameIdString[50] = {0};
+
 
 int checkIfCellIsInWinningSequence(Cell cell) {
 	for (int i = 0; i < 4; i++) {
-		if (cell.x == winningSequence[i].x && cell.y == winningSequence[i].y)
+		if (cell.x == winningSequence[i].x && cell.y == winningSequence[i].y) {
 			return IN_WINNING_SEQUENCE;
+		}
 	}
 	return NOT_IN_WINNING_SEQUENCE;
 }
 
-char calculateSignToDisplay(int x, int y, char onBoard, int displayWin) {
-	char result = onBoard;
-	int cellIsInWinningSequence;
-	if (displayWin) {
-		Cell cell;
-		cell.x = x;
-		cell.y = y;
 
-		cellIsInWinningSequence = checkIfCellIsInWinningSequence(cell);
-		if (cellIsInWinningSequence)
-			result = 'Y';
+void getGameId(void) {
+	char pathToPreviousGameId[100] = "..\\storage\\previousGameID.txt";
+
+	FILE *previousGameIdFile = fopen(pathToPreviousGameId, "r");
+	fscanf(previousGameIdFile, "%llu", &gameID);
+	fclose(previousGameIdFile);
+
+	gameID++;
+	gameIdIsAcquired = 1;
+}
+
+void updateGameId(void) {
+	char pathToPreviousGameId[100] = "..\\storage\\previousGameID.txt";
+
+	FILE *previousGameIdFile = fopen(pathToPreviousGameId, "w");
+	fprintf(previousGameIdFile, "%llu", gameID);
+
+	fclose(previousGameIdFile);
+}
+
+void convertGameIdNumberToString(void) {
+	snprintf(gameIdString, 50, "%llu", gameID);
+}
+
+
+void getAndUpdateGameId(void) {
+	getGameId();
+	convertGameIdNumberToString();
+	updateGameId();
+}
+
+char calculateSignToDisplay(Cell cellOnBoard, int win) {
+	// This function decides what sign to show: which is on the board ('O' or 'X') or 'Y' which indicates that smb won
+	int cellIsInWinningSequence = checkIfCellIsInWinningSequence(cellOnBoard);
+	if (win && cellIsInWinningSequence) {
+		return SIGN_IN_WINNING_SEQUENCE;
 	}
-	return result;
+
+	return cellOnBoard.sign;
 }
 
 int displayBoard(char board[BOARD_HEIGHT][BOARD_WIDTH], int displayWin) {
 	char signToDisplay;
+	char signOnBoard;
+	Cell cell;
 	printf("-------\n");
 	for (int i = 0; i < BOARD_HEIGHT; i++) {
 		for (int j = 0; j < BOARD_WIDTH; j++) {
-			signToDisplay = calculateSignToDisplay(j, i, board[i][j], displayWin);
+			signOnBoard = board[i][j];
+			cell = makeCell(signOnBoard, j, i);
+			signToDisplay = calculateSignToDisplay(cell, displayWin);
 			printf("%c", signToDisplay);
 		}
 		printf("\n");
@@ -68,6 +128,8 @@ char chooseSignBasedOnTurn(int turn) {
 			return 'X';
 		case -1:
 			return 'O';
+		default:
+			return '?';
 	}
 }
 
@@ -82,35 +144,75 @@ int findFirstEmptyCellFromBottomOfColumn(char board[BOARD_HEIGHT][BOARD_WIDTH], 
 	int cellIsEmpty = 0;;
 	for (int emptyCellLineNumber = BOARD_HEIGHT-1; emptyCellLineNumber >= 0 ; emptyCellLineNumber--) {
 		cellIsEmpty = checkIfCellIsEmpty(board[emptyCellLineNumber][column]);
-		if (cellIsEmpty)
+		if (cellIsEmpty) {
 			return emptyCellLineNumber;
+		}
 	}
 	return ILLEGAL_MOVE_FULL_COLUMN;
 }
 
-int putDotOnField(char board[BOARD_HEIGHT][BOARD_WIDTH], int line, int column, int turn) {
+void putDotOnField(char board[BOARD_HEIGHT][BOARD_WIDTH], int line, int column, int turn) {
 	char sign = chooseSignBasedOnTurn(turn);
 	board[line][column] = sign;
 
-	return SUCCESSFUL_EXECUTION;
 }
-int saveGame() {}
-int getPlayerName(char playersNames[PLAYER_COUNT][MAX_PLAYER_NAME_LENGTH], int playerNumeral) {
+
+int countEmptyCells(char board[BOARD_HEIGHT][BOARD_WIDTH]) {
+	int emptyCells = 0;
+	int cellIsEmpty = 0;
+	for (int i = 0; i < BOARD_HEIGHT; i++) {
+		for (int j = 0; j < BOARD_WIDTH; j++) {
+			cellIsEmpty = checkIfCellIsEmpty(board[i][j]);
+			emptyCells += cellIsEmpty;
+		}
+	}
+
+	return emptyCells;
+}
+
+void saveGame(char playersNames[PLAYER_COUNT][MAX_PLAYER_NAME_LENGTH], char board[BOARD_HEIGHT][BOARD_WIDTH], int turn) {
+	char pathToGames[100] = "..\\storage\\games\\";
+
+	if (!gameIdIsAcquired) {
+		getAndUpdateGameId();
+	}
+	printf("%d", gameID);
+
+	strcat(pathToGames, gameIdString);
+	strcat(pathToGames, ".txt");
+
+	printf("%s\n", pathToGames);
+
+	int emptyCells = countEmptyCells(board);
+
+	FILE *gameFile = fopen(pathToGames, "w");
+	fprintf(gameFile, "%llu\n%s\n%s\n%d\n", gameID, playersNames[0], playersNames[1], turn);
+
+	for (int i = 0; i < BOARD_HEIGHT; i++) {
+		for (int j = 0; j < BOARD_WIDTH; j++) {
+			fprintf(gameFile, "%c", board[i][j]);
+		}
+		fprintf(gameFile, "\n");
+	}
+
+
+	fclose(gameFile);
+}
+
+void getPlayerName(char playersNames[PLAYER_COUNT][MAX_PLAYER_NAME_LENGTH], int playerNumeral) {
 	// Gets name of only one player per call
 
 	printf("Please enter player#%d name (max 30 symbols): \n", playerNumeral);
 	scanf("%s", playersNames[playerNumeral]);
 
-	return SUCCESSFUL_EXECUTION;
 }
-int getPlayersNames(char playersNames[PLAYER_COUNT][MAX_PLAYER_NAME_LENGTH]) {
+void getPlayersNames(char playersNames[PLAYER_COUNT][MAX_PLAYER_NAME_LENGTH]) {
 	// Gets names of many players
 
 	for (int i = 0; i < PLAYER_COUNT; i++) {
 		getPlayerName(playersNames, i);
 	}
 
-	return SUCCESSFUL_EXECUTION;
 }
 
 int initializeBoard(char board[BOARD_HEIGHT][BOARD_WIDTH]) {
@@ -131,11 +233,14 @@ int displayPlayersNames(char playersNames[PLAYER_COUNT][MAX_PLAYER_NAME_LENGTH])
 	return SUCCESSFUL_EXECUTION;
 }
 
-int saveResult() {
-	printf("The game has been successfully saved!\n");
+void saveResult(char playersNames[PLAYER_COUNT][MAX_PLAYER_NAME_LENGTH], int gameResult) {
+	char pathToResults[100] = "..\\storage\\results";
+	strcat(pathToResults, ".txt");
 
+	FILE *resultFile = fopen(pathToResults, "a");
+	fprintf(resultFile, "ID: %s, 'X' player name: %s, 'O' player name: %s, result: %d\n", gameIdString, playersNames[0], playersNames[1], gameResult);
 
-	return SUCCESSFUL_EXECUTION;
+	fclose(resultFile);
 }
 int getPlayerProperInput() {
 	int input = -1;
@@ -156,53 +261,72 @@ int initializeGame(char playersNames[PLAYER_COUNT][MAX_PLAYER_NAME_LENGTH], char
 	getPlayersNames(playersNames);
 	initializeBoard(board);
 	displayPlayersNames(playersNames);
-	// displayBoard(board);
 
 	return SUCCESSFUL_EXECUTION;
 }
 
-int checkIfConnectedFour(char sign) {
-	char cell;
+int checkIfFourCharactersInWinningSequenceAreSame(char sign) {
+	// Checks if all the cells in 'winningSequence' array have the same character ('X' or 'Y')
+
 	for (int i = 0; i < 4; i++) {
-		cell = sequenceOnCheck[i];
-		if (cell != sign) {
+		if (winningSequence[i].sign != sign) {
 			return NOT_CONNECTED;
 		}
 	}
 	return CONNECTED;
 }
 
-void formHorizontalSequence(char board[BOARD_HEIGHT][BOARD_WIDTH], char sign, Cell startingCellInLine) {
-	for (int i = 0; i < 4; i++) {
-		sequenceOnCheck[i] = board[startingCellInLine.y][startingCellInLine.x + i];
+void formHorizontalSequence(char board[BOARD_HEIGHT][BOARD_WIDTH], Cell startingCellInSequence) {
+	// Fills 'winningSequence' array with four horizontally consecutive cells
 
-		winningSequence[i].x = startingCellInLine.x + i;
-		winningSequence[i].y = startingCellInLine.y;
+	int y = startingCellInSequence.y;
+	int x = startingCellInSequence.x;
+
+	for (int i = 0; i < 4; i++) {
+		winningSequence[i].sign = board[y][x + i];
+
+		winningSequence[i].x = x + i;
+		winningSequence[i].y = y;
 	}
 }
 
-void formVerticalSequence(char board[BOARD_HEIGHT][BOARD_WIDTH], char sign, Cell startingCell) {
-	for (int i = 0; i < 4; i++) {
-		sequenceOnCheck[i] = board[startingCell.y + i][startingCell.x];
+void formVerticalSequence(char board[BOARD_HEIGHT][BOARD_WIDTH], Cell startingCellInSequence) {
+	// Fills 'winningSequence' array with four vertically consecutive cells
 
-		winningSequence[i].x = startingCell.x;
-		winningSequence[i].y = startingCell.y + i;
+	int y = startingCellInSequence.y;
+	int x = startingCellInSequence.x;
+
+	for (int i = 0; i < 4; i++) {
+		winningSequence[i].sign = board[y + i][x];
+
+		winningSequence[i].x = x;
+		winningSequence[i].y = y + i;
 	}
 }
-void formDiagonalTangentNegativeSequence(char board[BOARD_HEIGHT][BOARD_WIDTH], char sign, Cell startingCell) {
-	for (int i = 0; i < 4; i++) {
-		sequenceOnCheck[i] = board[startingCell.y + i][startingCell.x + i];
+void formDiagonalTangentNegativeSequence(char board[BOARD_HEIGHT][BOARD_WIDTH], Cell startingCellInSequence) {
+	// Fills 'winningSequence' array with four diagonally (whose slope is negative) consecutive cells
 
-		winningSequence[i].x = startingCell.x + i;
-		winningSequence[i].y = startingCell.y + i;
+	int y = startingCellInSequence.y;
+	int x = startingCellInSequence.x;
+
+	for (int i = 0; i < 4; i++) {
+		winningSequence[i].sign = board[y + i][x + i];
+
+		winningSequence[i].x = x + i;
+		winningSequence[i].y = y + i;
 	}
 }
-void formDiagonalTangentPositiveSequence(char board[BOARD_HEIGHT][BOARD_WIDTH], char sign, Cell startingCell) {
-	for (int i = 0; i < 4; i++) {
-		sequenceOnCheck[i] = board[startingCell.y - i][startingCell.x + i];
+void formDiagonalTangentPositiveSequence(char board[BOARD_HEIGHT][BOARD_WIDTH], Cell startingCellInSequence) {
+	// Fills 'winningSequence' array with four diagonally (whose slope is positive) consecutive cells
 
-		winningSequence[i].x = startingCell.x + i;
-		winningSequence[i].y = startingCell.y - i;
+	int y = startingCellInSequence.y;
+	int x = startingCellInSequence.x;
+
+	for (int i = 0; i < 4; i++) {
+		winningSequence[i].sign = board[y - i][x + i];
+
+		winningSequence[i].x = x + i;
+		winningSequence[i].y = y - i;
 	}
 }
 
@@ -220,65 +344,72 @@ int checkWinCondition(char board[BOARD_HEIGHT][BOARD_WIDTH], int turn) {
 		startingCell.y = i;
 		for (int j = 0; j < BOARD_WIDTH; j++) {
 			startingCell.x = j;
+			startingCell.sign = board[i][j];
 			if (j < 4) {
-				formHorizontalSequence(board, sign, startingCell);
-				connectedFourHorizontally = checkIfConnectedFour(sign);
-				if (connectedFourHorizontally)
+				formHorizontalSequence(board, startingCell);
+				connectedFourHorizontally = checkIfFourCharactersInWinningSequenceAreSame(sign);
+				if (connectedFourHorizontally) {
 					return WIN;
+				}
 			}
 
-			if (i < 4) {
-				formVerticalSequence(board, sign, startingCell);
-				connectedFourVertically = checkIfConnectedFour(sign);
-				if (connectedFourVertically)
+			if (i < 3) {
+				formVerticalSequence(board, startingCell);
+				connectedFourVertically = checkIfFourCharactersInWinningSequenceAreSame(sign);
+				if (connectedFourVertically) {
 					return WIN;
+				}
 			}
 
-			if (i < 4 && j < 4) {
-				formDiagonalTangentNegativeSequence(board, sign, startingCell);
-				connectedFourDiagonallyTangentNegative = checkIfConnectedFour(sign);
-				if (connectedFourDiagonallyTangentNegative)
+			if (i < 3 && j < 4) {
+				formDiagonalTangentNegativeSequence(board, startingCell);
+				connectedFourDiagonallyTangentNegative = checkIfFourCharactersInWinningSequenceAreSame(sign);
+				if (connectedFourDiagonallyTangentNegative) {
 					return WIN;
+				}
 			}
 
 			if (i >= 3 && j < 4) {
-				formDiagonalTangentPositiveSequence(board, sign, startingCell);
-				connectedFourDiagonallyTangentPositive = checkIfConnectedFour(sign);
-				if (connectedFourDiagonallyTangentPositive)
+				formDiagonalTangentPositiveSequence(board, startingCell);
+				connectedFourDiagonallyTangentPositive = checkIfFourCharactersInWinningSequenceAreSame(sign);
+				if (connectedFourDiagonallyTangentPositive) {
 					return WIN;
+				}
 			}
 		}
 	}
 	return NO_WIN;
 }
 int checkDrawCondition(char board[BOARD_HEIGHT][BOARD_WIDTH]) {
-
-
 	int cellIsEmpty = 0;
 	for (int i = 0; i < BOARD_HEIGHT; i++) {
 		for (int j = 0; j < BOARD_WIDTH; j++) {
 			cellIsEmpty = checkIfCellIsEmpty(board[i][j]);
-			if (cellIsEmpty)
+			if (cellIsEmpty) {
 				return NO_DRAW;
+			}
 		}
 	}
+
 	return DRAW;
 }
 
 int checkGameEndConditions(char board[BOARD_HEIGHT][BOARD_WIDTH], int turn) {
 	int win = checkWinCondition(board, turn);
-	if (win)
+	if (win) {
 		return WIN * turn;
+	}
 
 	int draw = checkDrawCondition(board);
-	if (draw)
+	if (draw) {
 		return DRAW;
+	}
 
 	return CONTINUE;
 }
 
 int simulateGame(char playersNames[PLAYER_COUNT][MAX_PLAYER_NAME_LENGTH], char board[BOARD_HEIGHT][BOARD_WIDTH]) {
-	// Game loop
+	// Game main loop
 
 	int turn = 1, playerInput = 0, gameEnd = 0, column = -1;
 	int emptyCellLineNumber = -1;
@@ -289,15 +420,14 @@ int simulateGame(char playersNames[PLAYER_COUNT][MAX_PLAYER_NAME_LENGTH], char b
 		playerInput = getPlayerProperInput();
 
 		if (playerInput == 0) {
-			saveGame(turn);
+			saveGame(playersNames, board, turn);
 			continue;
 		}
 
 		column = playerInput-1;
-
 		emptyCellLineNumber = findFirstEmptyCellFromBottomOfColumn(board, column);
 		if (emptyCellLineNumber == ILLEGAL_MOVE_FULL_COLUMN) {
-			printf("Illegal move: there is no free space in this column\n");
+			printf("Illegal move: there is no free space in this column");
 			continue;
 		}
 
@@ -307,10 +437,9 @@ int simulateGame(char playersNames[PLAYER_COUNT][MAX_PLAYER_NAME_LENGTH], char b
 		if (gameEnd) {
 			return gameEnd;
 		}
-		// printf("Game has not been finished yet\n");
+
 		turn *= (-1);
 	}
-	return SUCCESSFUL_EXECUTION;
 }
 
 
@@ -335,23 +464,39 @@ int getProperInput() {
 	return input;
 }
 
+void displayBoardBaseOnGameResult(char playersNames[PLAYER_COUNT][MAX_PLAYER_NAME_LENGTH], char board[BOARD_HEIGHT][BOARD_WIDTH], int gameResult) {
+	switch (gameResult) {
+		case WIN:
+			displayBoard(board, WIN);
+			printf("The game has ended. Player %s (#1) has won!\n", playersNames[0]);
+			break;
+		case (-WIN):
+			displayBoard(board, WIN);
+			printf("The game has ended. Player %s (#2) has won!\n", playersNames[1]);
+			break;
+		case (DRAW):
+			displayBoard(board, NO_WIN);
+			printf("The game has ended. NO WINNERS TODAY!\n");
+			break;
+	}
+}
+
+
+
 void playGame(char playersNames[PLAYER_COUNT][MAX_PLAYER_NAME_LENGTH], char board[BOARD_HEIGHT][BOARD_WIDTH]) {
 	initializeGame(playersNames, board);
 
 	int gameResult = simulateGame(playersNames, board);
-	if (gameResult == WIN) {
-		displayBoard(board, WIN);
-		printf("The game has ended. Player %s (#1) has won!", playersNames[0]);
-	} else if (gameResult == -WIN) {
-		displayBoard(board, WIN);
-		printf("The game has ended. Player %s (#2) has won!", playersNames[1]);
-	} else {
-		displayBoard(board, NO_WIN);
-		printf("The game has ended. NO WINNERS TODAY!", playersNames[1]);
-	}
+	displayBoardBaseOnGameResult(playersNames, board, gameResult);
 
-	saveResult();
+	if (!gameIdIsAcquired) {
+		getAndUpdateGameId();
+	}
+	saveResult(playersNames, gameResult);
 }
+
+
+
 
 void loadGame() {
 	printf("We loaded a game. Now what?\n");
@@ -364,6 +509,7 @@ void exitGame() {
 int main(void) {
 	char board[BOARD_HEIGHT][BOARD_WIDTH];
 	char playersNames[PLAYER_COUNT][MAX_PLAYER_NAME_LENGTH];
+	char placeHolder = 'a';
 
 	displayMainMenu();
 	int inputOption = getProperInput();
@@ -371,13 +517,13 @@ int main(void) {
 	switch (inputOption) {
 		case 1:
 			playGame(playersNames, board);
-		break;
+			break;
 		case 2:
 			loadGame();
-		break;
+			break;
 		case 3:
 			exitGame();
-		exit(SUCCESSFUL_EXECUTION);
 	}
+	scanf("%c", &placeHolder);
 	return SUCCESSFUL_EXECUTION;
 }
